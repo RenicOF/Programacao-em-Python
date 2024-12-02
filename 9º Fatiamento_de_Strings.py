ac52f06e26643745441674a7cdae92b7df6388e3
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-02 10:32:39 -0300

    Create 9º Fatiamento_de_Strings.py

---
+++ 9º Fatiamento_de_Strings.py
@@ -0,0 +1,14 @@
+#Strings são listas de bytes represetando caracteres.
+#Podemos acessar suas posições usando Colchetes
+#String [Posição Inicial: Posição Final]
+
+# Strings
+Minha_String = 'Aprender python é Top!'
+print( type(Minha_String) )
+print( len(Minha_String) )
+
+print( Minha_String[0] )
+print( Minha_String[5] )
+print( Minha_String[-1] )
+print( Minha_String[-10:] )
+print( Minha_String[0:10] )
