7242abc6846eb6e82ec11e9044be525a1d189b6a
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-02 10:22:24 -0300

    Create 1. - Sintaxe_básica.py

---
+++ 1. - Sintaxe_básica.py
@@ -0,0 +1,8 @@
+#Podemos utilziar ' ' (aspas simples)
+#Podemos utilziar " " (aspas duplas)
+#Utilizando o \n para pular uma linha na impressão do valor
+#Usamos o hastgag # para comentar algo
+
+# Sintaxe básica
+print('Olá Mundão!!! \n')
+print("Olá Mundão!!!")
