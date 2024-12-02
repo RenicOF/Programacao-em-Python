0a821a57d05bf610a0d377e5978b5b0c670d15ff
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-02 10:24:04 -0300

    Create 2 . - Operadores_matemáticos.py

---
+++ 2 . - Operadores_matemáticos.py
@@ -0,0 +1,20 @@
+#Os operadores são usados ​​para realizar operações em variáveis ​​e valores:
+# Operadores para cálculos
+Soma = 1 + 2
+Subtração = 1 - 1
+Multiplicação = 2 * 10
+Divisão = 10 / 2
+Exponenciação = 2 ** 2
+Resto_Divisao = 9 % 4
+Divisao_Chão = 9 // 4
+
+# Print formatado
+print( 'Somar:{}'.format(Soma) ) 
+print( 'Subtração:', Subtração ) 
+print( f'Multiplacação: {Multiplicação}' )
+
+print( 'Divisão', Divisão ) 
+print( 'Exponenciação: {}' .format(Exponenciação) )
+print( 'Resto_Divisao', Resto_Divisao )
+print( 'Divisao_Chão: {}'.format(Divisao_Chão) )
+print( 'Varios elementos: {}, {}, {}'.format(Soma, Subtração, Divisão) )
