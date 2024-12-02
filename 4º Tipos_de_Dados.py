c2f77fc486dc80e100f92cf15bef548d70490970
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-02 10:26:14 -0300

    Create 4º Tipos_de_Dados.py

---
+++ 4º Tipos_de_Dados.py
@@ -0,0 +1,25 @@
+#Listas - São criadas usando [ ] (Colchetes)
+#Tuplas - São criadas usando ( ) (Parenteses)
+#Dicionários - São criadas usando { } (Chaves)
+#Esses tipos de dados podem receber qualquer tipo de informação, inclusive Operações Aritméticas
+
+# Tipos de Dados
+Lista_Exemplo_1 = [1, 2, 3, 4, 5]
+Lista_Exemplo_2 = ['Nome', 'Quarto', 'Morango', 4, 5]
+Lista_Exemplo_3 = [1, 'Quarto', True, False]
+
+# Tuplas são imutaveis (Não modifica)
+Tupla_Exemplo_1 = ( 2, 3, 4, 6 )
+Tupla_Exemplo_2 = ( 'Odemir', 'Depieri', 4 )
+
+# Dicionário são para organizar os dados (Chave, Valor)
+Dicionario = { 'Nome':'Odemir', 'Idade':29 }
+
+print( Lista_Exemplo_1 )
+print( Lista_Exemplo_2 )
+print( Lista_Exemplo_3, '\n' )
+
+print( Tupla_Exemplo_1 )
+print( Tupla_Exemplo_2, '\n' )
+
+print( Dicionario )
