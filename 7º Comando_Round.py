22b0e3785400e8302f33bdef932d8a1ef34c41c8
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-02 10:30:34 -0300

    Create 7º Comando_Round.py

---
+++ 7º Comando_Round.py
@@ -0,0 +1,11 @@
+#Quando trabalhamos com números flutuantes, podemos arredondar o valor usando um comando nativo do Python round( valor, número de casas)
+
+# Comando Round
+# Metodo round, arredonda valores
+
+Valor_Exemplo = 12.123456789
+
+print( round( Valor_Exemplo, 1 ) )
+print( round( Valor_Exemplo, 2 ) )
+print( round( Valor_Exemplo, 3 ) )
+print( round( Valor_Exemplo, 5 ) )
