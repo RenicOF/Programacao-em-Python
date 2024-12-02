18f098958f92317ccf916fcf8cf5df1bf51b43d7
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-02 10:31:31 -0300

    Create 8º Comando_Len.py

---
+++ 8º Comando_Len.py
@@ -0,0 +1,11 @@
+#A função len() retorna a quantidade de elementos de qualquer lista em Python, essa é mais uma função built-in e disponível em qualquer parte dos nossos códigos.
+
+Lista = [1, 2, 3, 4, 5]
+Dicionario = { 'Nome':'Odemir', 'Idade':29, 'Vip':False }
+Tupla = (10, 20)
+String = 'Olá Mundão!'
+
+print( len(Lista) )
+print( len(Dicionario) )
+print( len(Tupla) )
+print( len(String) )
