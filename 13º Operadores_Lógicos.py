fa3bfade21936f99d0db49c30cc2de05689723af
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-04 09:18:48 -0300

    Create 13º Operadores_Lógicos.py

---
+++ 13º Operadores_Lógicos.py
@@ -0,0 +1,9 @@
+# Operadores de Comparações - Parte 2
+And = 'and'
+Ou = 'or'
+Negação = 'not'
+
+print('8 é maior que 7 e 7 maior que 8:', 8 > 7 and 7 > 8 )
+print('8 é maior que 7 e 7 menor que 8:', 8 > 7 and 7 < 8 )
+print('8 é maior que 7 ou 7 menor que 8:', 8 == 7 or 7 == 8 )
+print('8 é maior que 7 ou 7 menor que 8:', 8 > 7 or 7 > 8 )
