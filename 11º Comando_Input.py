618aae8c8a367c63dbc0fb11341dc9f018202af1
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-04 09:13:03 -0300

    Create 11º Comando_Input.py

---
+++ 11º Comando_Input.py
@@ -0,0 +1,8 @@
+# Comando input
+# Enviar uma informação momentânea
+
+Nome = input('Qual seu nome? ')
+Idade = input('Quantos anos você tem? ')
+
+print('Seu nome é:', Nome)
+print('Sua idade:', Idade)
