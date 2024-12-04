1a13a2c07c7aff65d7fe71621adcce51408494b4
RenicOF <151588826+RenicOF@users.noreply.github.com>
2024-12-04 09:23:50 -0300

    Create 15º Operadores_de_Associação.py

---
+++ 15º Operadores_de_Associação.py
@@ -0,0 +1,9 @@
+#Os operadores de associação são usados ​​para testar se uma sequência é apresentada em um objeto:
+
+#in (Retorna True se uma sequência com o valor especificado estiver presente no objeto)
+#not in (Retorna True se uma sequência com o valor especificado não estiver presente no objeto)
+
+Lista_Ações = ['Magalu', 'Via', 'Assai', 'Carrefour']
+
+print( 'Via' in Lista_Ações )
+print( 'Via' not in Lista_Ações )
